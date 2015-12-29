0b7211ae3afaa89a4ce761fa4cffa0e3bf0b40a1
Lukáš Stankovič <lukasstankovic@gmail.com>
2015-12-29 15:54:09 +0100

    Změna menu

--- sablona/hlavicka.php
+++ sablona/hlavicka.php
@@ -4,7 +4,7 @@
                     <a class="logo" href="index.php"><img src="img/logo.png" alt="logo" height="80"></a>
                     <ul class="hlavni-navigace">
                         <li><a href="#">O nás</a></li>
-                        <li><a href="#">Kontakt</a></li>
+                        <li><a href="#">Vyvolat</a></li>
                     </ul>
                 </nav>
             </div>
