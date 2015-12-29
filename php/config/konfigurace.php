b7f5f6d97c02cba0c34ebc1ceaa2522f7cab347f
Lukáš Stankovič <lukasstankovic@gmail.com>
2015-12-29 12:48:47 +0100

    Upravená konfigurace i pro web

--- php/config/konfigurace.php
+++ php/config/konfigurace.php
@@ -1,12 +1,29 @@
 <?php
 include_once ("php/tridy/databaze.php");
-include_once ("php/tridy/uzivatele.php");
+include_once ("php/tridy/clanky.php");
 include_once ("php/tridy/objednavky.php");
 include_once ("php/tridy/fotky.php");
+include_once ("php/tridy/uzivatele.php");
+
+//PARAMETRY
+include_once ("php/tridy/formaty.php");
+include_once ("php/tridy/desky.php");
+include_once ("php/tridy/fotopapiry.php");
+include_once ("php/tridy/materialy.php");
+include_once ("php/tridy/typy.php");
 
 
-$Uzivatele = new Uzivatele();
+$Clanky = new Clanky();
 $Objednavky = new Objednavky();
+$Uzivatele = new Uzivatele();
 $Fotky = new Fotky();
+
+//PARAMETRY
+$Formaty = new Formaty();
+$Desky = new Desky();
+$Fotopapiry = new Fotopapiry();
+$Materialy = new Materialy();
+$Typy = new Typy();
+
 
 ?>
