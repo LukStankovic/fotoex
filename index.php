bbd8f366b530aaa8c5ca5a8142fcbd5d85fc8236
Lukáš Stankovič <lukasstankovic@gmail.com>
2015-12-29 14:18:14 +0100

    Prozatímní výpisy článků

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 include_once("sablona/head.php");
 //VLOZENI headeru, loga a menu
 include_once("sablona/hlavicka.php");
+$clanky = $Clanky->vse();
 ?>
 
         <main class="uvod">
@@ -16,37 +17,14 @@ include_once("sablona/hlavicka.php");
                     </div>       
                 </div>
                 <div class="obsah">
-                    <h1>Novinky ze světa fotografií</h1>
-                    <article>
-                        <h2>Lorem ipsum</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque bibendum molestie fermentum. Mauris feugiat dictum nisl, a maximus ex dictum rhoncus. Cras faucibus bibendum massa vel malesuada. Ut vitae facilisis sapien. Suspendisse ultricies erat vel nibh faucibus suscipit. Cras id ligula justo. Nam laoreet ut neque ac gravida. Phasellus malesuada venenatis felis, ut viverra ante bibendum id. Vestibulum imperdiet augue eget blandit feugiat.</p>
-<p>
-Ut aliquam nulla non metus ullamcorper, et placerat tortor elementum. Nullam ac ligula in nulla gravida sagittis. In id vehicula justo, non efficitur nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper urna et urna aliquet, et gravida urna molestie. Nam ornare et dui eget tincidunt. Suspendisse mauris lorem, mattis a imperdiet ac, porttitor sed tortor. Etiam congue leo vitae scelerisque eleifend. Etiam bibendum eros in ante varius vehicula. Nullam et semper mauris, quis cursus magna. Vivamus vel feugiat orci, non hendrerit velit. Aenean placerat dolor sapien, et gravida mauris elementum ut. Suspendisse id ornare tortor. Nulla facilisi. Aenean suscipit, arcu eu vulputate finibus, neque leo rutrum neque, ac luctus mauris metus vitae mauris. Integer congue ipsum tortor, id eleifend turpis vestibulum ut.
-</p>
-<p>
-Sed sed augue velit. Ut ultrices in sem et luctus. Vestibulum ac velit leo. Cras volutpat non quam ac pellentesque. Suspendisse eget malesuada ante. Integer quis urna felis. Sed ultricies, mauris et euismod feugiat, nisl nulla facilisis dui, id tincidunt augue lorem ac nibh. In hac habitasse platea dictumst. Pellentesque hendrerit ipsum mauris, eget laoreet lacus commodo eu. Quisque dapibus, arcu in suscipit ullamcorper, enim nunc tincidunt lorem, et ultricies nulla enim eget justo. Donec ex libero, bibendum vel iaculis et, luctus ac arcu. Curabitur vel rutrum metus. Fusce volutpat, est quis tincidunt porttitor, erat lacus facilisis neque, non interdum ante mi id lorem.
-                   </p>
-                    </article>
-                    <article>
-                        <h2>Lorem ipsum</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque bibendum molestie fermentum. Mauris feugiat dictum nisl, a maximus ex dictum rhoncus. Cras faucibus bibendum massa vel malesuada. Ut vitae facilisis sapien. Suspendisse ultricies erat vel nibh faucibus suscipit. Cras id ligula justo. Nam laoreet ut neque ac gravida. Phasellus malesuada venenatis felis, ut viverra ante bibendum id. Vestibulum imperdiet augue eget blandit feugiat.</p>
-<p>
-Ut aliquam nulla non metus ullamcorper, et placerat tortor elementum. Nullam ac ligula in nulla gravida sagittis. In id vehicula justo, non efficitur nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper urna et urna aliquet, et gravida urna molestie. Nam ornare et dui eget tincidunt. Suspendisse mauris lorem, mattis a imperdiet ac, porttitor sed tortor. Etiam congue leo vitae scelerisque eleifend. Etiam bibendum eros in ante varius vehicula. Nullam et semper mauris, quis cursus magna. Vivamus vel feugiat orci, non hendrerit velit. Aenean placerat dolor sapien, et gravida mauris elementum ut. Suspendisse id ornare tortor. Nulla facilisi. Aenean suscipit, arcu eu vulputate finibus, neque leo rutrum neque, ac luctus mauris metus vitae mauris. Integer congue ipsum tortor, id eleifend turpis vestibulum ut.
-</p>
-<p>
-Sed sed augue velit. Ut ultrices in sem et luctus. Vestibulum ac velit leo. Cras volutpat non quam ac pellentesque. Suspendisse eget malesuada ante. Integer quis urna felis. Sed ultricies, mauris et euismod feugiat, nisl nulla facilisis dui, id tincidunt augue lorem ac nibh. In hac habitasse platea dictumst. Pellentesque hendrerit ipsum mauris, eget laoreet lacus commodo eu. Quisque dapibus, arcu in suscipit ullamcorper, enim nunc tincidunt lorem, et ultricies nulla enim eget justo. Donec ex libero, bibendum vel iaculis et, luctus ac arcu. Curabitur vel rutrum metus. Fusce volutpat, est quis tincidunt porttitor, erat lacus facilisis neque, non interdum ante mi id lorem.
-                   </p>
-                    </article>
-                                        <article>
-                        <h2>Lorem ipsum</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque bibendum molestie fermentum. Mauris feugiat dictum nisl, a maximus ex dictum rhoncus. Cras faucibus bibendum massa vel malesuada. Ut vitae facilisis sapien. Suspendisse ultricies erat vel nibh faucibus suscipit. Cras id ligula justo. Nam laoreet ut neque ac gravida. Phasellus malesuada venenatis felis, ut viverra ante bibendum id. Vestibulum imperdiet augue eget blandit feugiat.</p>
-<p>
-Ut aliquam nulla non metus ullamcorper, et placerat tortor elementum. Nullam ac ligula in nulla gravida sagittis. In id vehicula justo, non efficitur nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper urna et urna aliquet, et gravida urna molestie. Nam ornare et dui eget tincidunt. Suspendisse mauris lorem, mattis a imperdiet ac, porttitor sed tortor. Etiam congue leo vitae scelerisque eleifend. Etiam bibendum eros in ante varius vehicula. Nullam et semper mauris, quis cursus magna. Vivamus vel feugiat orci, non hendrerit velit. Aenean placerat dolor sapien, et gravida mauris elementum ut. Suspendisse id ornare tortor. Nulla facilisi. Aenean suscipit, arcu eu vulputate finibus, neque leo rutrum neque, ac luctus mauris metus vitae mauris. Integer congue ipsum tortor, id eleifend turpis vestibulum ut.
-</p>
-<p>
-Sed sed augue velit. Ut ultrices in sem et luctus. Vestibulum ac velit leo. Cras volutpat non quam ac pellentesque. Suspendisse eget malesuada ante. Integer quis urna felis. Sed ultricies, mauris et euismod feugiat, nisl nulla facilisis dui, id tincidunt augue lorem ac nibh. In hac habitasse platea dictumst. Pellentesque hendrerit ipsum mauris, eget laoreet lacus commodo eu. Quisque dapibus, arcu in suscipit ullamcorper, enim nunc tincidunt lorem, et ultricies nulla enim eget justo. Donec ex libero, bibendum vel iaculis et, luctus ac arcu. Curabitur vel rutrum metus. Fusce volutpat, est quis tincidunt porttitor, erat lacus facilisis neque, non interdum ante mi id lorem.
-                   </p>
-                    </article>
+                   <h1>Novinky ze světa fotografií</h1>
+                   
+                   <?php  echo "<pre>",print_r($clanky),"</pre>";?>
+                   <?php foreach($clanky as $clanek){ ?>
+
+                       
+                       </article>
+                    <?php } ?>
                 </div> 
             </div>
 
