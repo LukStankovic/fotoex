dfe0a32127e9702759679984425e35399e310ea2
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-06 19:48:20 +0100

    I. Změna designu - cover

--- index.php
+++ index.php
@@ -6,43 +6,11 @@ include_once("sablona/hlavicka.php");
 $clanky = $Clanky->vse();
 ?>
 
-        <main class="uvod">
-            <div class="container stranka">
-                <div class="uvod-banner">
-                    <div class="row">
-                        <div class="col-md-9">
-                            Využijte náš moderní způsob pro vyvolání Vašich fotografií z domova. Vše Vám zabere jen pár minut.
-                        </div>
-                        <div class="col-md-3 tlacitko"><a href="nahrani.php" class="btn">Vyvolat fotografie</a></div> 
-                    </div>       
-                </div>
-                <div class="obsah">
-                    <h1>Novinky ze světa fotografií</h1>
-                    <?php foreach($clanky as $clanek){ ?>
-                    <article>
-                         
-                        <div class="cover">
-                            <a href="#"><img src="clanky/<?php echo $clanek->cover; ?>"></a>
-                        </div>
-                        <div class="clanek">
-                            <h2><a href="#"><?php echo $clanek->nazev; ?></a></h2>
-                            <div class="info"><i class="fa fa-user"></i><?php echo $clanek->jmeno." ".$clanek->prijmeni; ?></div>
-                            <div class="info"><i class="fa fa-calendar"></i> <?php echo date( 'd. m. Y', strtotime($clanek->datum));?></div>
-                            <div class="text">
-                                <?php echo substr($clanek->obsah,0,400)."..."; ?>    
-                            </div>
-                            
-                        </div>
-                    </article>
-                    <?php } ?>                   
-                   
-                   
-                   
-
-                </div> 
-            </div>
-
-        </main>
+<main class="uvod">              
+    <div class="container stranka">
+        <h1>Lorem ipsum</h1>
+    </div>
+</main>
         
 <?php 
 //VLOZENI PATICKY
--- sablona/hlavicka.php
+++ sablona/hlavicka.php
@@ -1,11 +1,16 @@
         <header>
             <div class="container">
                 <nav>
-                    <a class="logo" href="index.php"><img src="img/logo.png" alt="logo" height="80"></a>
-                    <ul class="hlavni-navigace">
-                        <li><a href="#">O nás</a></li>
-                        <li><a href="#">Vyvolat</a></li>
+                    <a class="logo pull-left" href="index.php"><img src="img/logo.png" alt="logo"></a>
+                    <ul class="hlavni-navigace pull-right">
+                        <li><a href="#">Vyvolat fotografie</a></li>
+                        <li><a href="#">Novinky</a></li>
+                        <li><a href="#">Kontakt</a></li>
                     </ul>
                 </nav>
+            </div>
+            <div class="uvodni-text">
+                Tiskárna fotografií nové generace
+                <div class="vyvolat"><a href="#" class="btn">Vyvolat fotografie</a></div>
             </div>
         </header>    
