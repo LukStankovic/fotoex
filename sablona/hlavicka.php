fa52c4ec2d0bac9e402e7b3e575edad6af4d6c3d
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-06 23:14:38 +0100

    Patička + mobilní menu

--- sablona/hlavicka.php
+++ sablona/hlavicka.php
@@ -1,16 +1,25 @@
         <header>
-            <div class="container">
-                <nav>
+                <nav class="container">
                     <a class="logo pull-left" href="index.php"><img src="img/logo.png" alt="logo"></a>
                     <ul class="hlavni-navigace pull-right">
                         <li><a href="#">Vyvolat fotografie</a></li>
-                        <li><a href="#">Novinky</a></li>
+                        <li><a href="#">O nás</a></li>
                         <li><a href="#">Kontakt</a></li>
                     </ul>
                 </nav>
-            </div>
+                <div class="mobilni-navigace container">
+                    <a class="mobilni-nav-tlacitko" role="button" data-toggle="collapse" href="#mobilni-menu" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-bars"></i></a>
+                    <div class="collapse" id="mobilni-menu">
+                        <ul>
+                            <li><a href="#">Vyvolat fotografie</a></li>
+                            <li><a href="#">O nás</a></li>
+                            <li><a href="#">Kontakt</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
             <div class="uvodni-text">
-                Tiskárna fotografií nové generace
+                <span class="intro">Tiskárna fotografií nové generace</span>
                 <div class="vyvolat"><a href="#" class="btn">Vyvolat fotografie</a></div>
             </div>
         </header>    
--- sablona/paticka.php
+++ sablona/paticka.php
@@ -1,8 +1,17 @@
         <footer>
-            
-            
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="foot-navigace">
+                            <ul>
+                                <li><a href="#">Vyvolat fotografie</a></li>
+                                <li><a href="#">O nás</a></li>
+                                <li><a href="#">Kontakty</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
-        
-
 	</body>
 </html>
