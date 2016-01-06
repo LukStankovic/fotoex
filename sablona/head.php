b376ac7204f90fa8c87c9e30eb6ea2ac54f6ad48
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-06 19:48:38 +0100

    Pročištění nepoužitých souborů

--- sablona/head.php
+++ sablona/head.php
@@ -7,23 +7,17 @@ include_once("php/config/konfigurace.php");
 	<head>
 		<meta charset="utf-8">
 		<title>FotoEx - Online fotolab pro Vaše fotografie</title>
-		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-       <!--<link rel="stylesheet" href="css/material.min.css">-->
-       <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600,400italic,300italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600,400italic,300italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
-        <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
         <link rel="stylesheet" href="css/multiuploader/jquery.fileupload.css">
         <link rel="stylesheet" href="css/multiuploader/jquery.fileupload-ui.css">
-        <!-- CSS adjustments for browsers with JavaScript disabled -->
         <noscript><link rel="stylesheet" href="css/multiuploader/jquery.fileupload-noscript.css"></noscript>
         <noscript><link rel="stylesheet" href="css/multiuploader/jquery.fileupload-ui-noscript.css"></noscript>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> 
  		<link rel="stylesheet" href="css/style.css">
  		
- 		
         <script src="js/jquery.min.js"></script>
-        <script src="js/material.min.js"></script>
 		<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
