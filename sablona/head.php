92e984979be8e66beb7dcb62206d2743f013a5d2
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-09 13:11:47 +0100

    Přidání bootstrapu do složky s webem

--- sablona/head.php
+++ sablona/head.php
@@ -15,7 +15,7 @@ include_once("php/config/konfigurace.php");
         <noscript><link rel="stylesheet" href="css/multiuploader/jquery.fileupload-noscript.css"></noscript>
         <noscript><link rel="stylesheet" href="css/multiuploader/jquery.fileupload-ui-noscript.css"></noscript>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="css/bootstrap.min.css"> 
  		<link rel="stylesheet" href="css/style.css">
  		
         <script src="js/jquery.min.js"></script>
