7c35da0c5d0586f47ec36b09ed6cdb78e88415b1
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-09 13:12:15 +0100

    Předělání div classu do main

--- detail-clanku.php
+++ detail-clanku.php
@@ -6,21 +6,19 @@ include_once("sablona/hlavicka.php");
 $clanek = $Clanky->detail($_GET["id"]);
 ?>
 
-<main id="clanek">              
-    <div class="container stranka">
-        <div class="uvod-clanku">
-            <h1><?php echo $clanek->nazev?></h1>
-            <div class="info">
-                <span class="datum"><i class="fa fa-calendar"></i><?php echo date("j. n. Y H:i",strtotime($clanek->datum)); ?></span>
-                <span class="autor"><i class="fa fa-user"></i><?php echo "$clanek->jmeno $clanek->prijmeni"; ?></span>
-            </div>
+<main id="clanek" class="container stranka">             
+    <div class="uvod-clanku">
+        <h1><?php echo $clanek->nazev?></h1>
+        <div class="info">
+            <span class="datum"><i class="fa fa-calendar"></i><?php echo date("j. n. Y H:i",strtotime($clanek->datum)); ?></span>
+            <span class="autor"><i class="fa fa-user"></i><?php echo "$clanek->jmeno $clanek->prijmeni"; ?></span>
         </div>
-        <div class="obsah">
-            <img src="clanky/<?php echo $clanek->cover;?>" alt="Úvodník článku">
-            <?php echo $clanek->obsah; ?>
-        </div>
-        <a href="index.php" class="btn pull-right"> Zpět na hlavní stránku</a>
     </div>
+    <div class="obsah">
+        <img src="clanky/<?php echo $clanek->cover;?>" alt="Úvodník článku">
+        <?php echo $clanek->obsah; ?>
+    </div>
+    <a href="index.php" class="btn pull-right"> Zpět na hlavní stránku</a>
 </main>
         
 <?php 
--- index.php
+++ index.php
@@ -7,30 +7,27 @@ $clanky = $Clanky->vse();
 $poc = 0;
 ?>
 
-<main>              
-    <div class="container stranka">
-        <?php foreach($clanky as $clanek){ ?>
-        <div class="clanek clanek-<?php echo $clanek->id_clanek; ?> row">
-           
-            <div class="col-md-6 fotografie">
-                <div class="uvodni-img" style="background: url(clanky/<?php echo $clanek->cover; ?>)"><a href="detail-clanku.php?id=<?php echo $clanek->id_clanek; ?>"></a></div>
-            </div>
-            <div class="col-md-6 obsah-clanku">
-                <h1><a href="detail-clanku.php?id=<?php echo $clanek->id_clanek; ?>"><?php echo $clanek->nazev; ?></a></h1>
-                <div class="info">
-                    <span class="datum"><i class="fa fa-calendar"></i><?php echo date("j. n. Y H:i",strtotime($clanek->datum)); ?></span>
-                    <span class="autor"><i class="fa fa-user"></i><?php echo "$clanek->jmeno $clanek->prijmeni"; ?></span>
-                </div>
-                <?php echo substr($clanek->obsah,0,400)."..."; ?>
-                <div class="cist-dale"><a href="detail-clanku.php?id=<?php echo $clanek->id_clanek; ?>" class="btn">Číst dále</a></div>
-            </div>
-
+<main class="container stranka">
+    <?php foreach($clanky as $clanek){ ?>
+    <div class="clanek clanek-<?php echo $clanek->id_clanek; ?> row">
+       
+        <div class="col-md-6 fotografie">
+            <div class="uvodni-img" style="background: url(clanky/<?php echo $clanek->cover; ?>)"><a href="detail-clanku.php?id=<?php echo $clanek->id_clanek; ?>"></a></div>
         </div>
-        <?php 
-        $poc++;
-        } 
-        ?>
+        <div class="col-md-6 obsah-clanku">
+            <h1><a href="detail-clanku.php?id=<?php echo $clanek->id_clanek; ?>"><?php echo $clanek->nazev; ?></a></h1>
+            <div class="info">
+                <span class="datum"><i class="fa fa-calendar"></i><?php echo date("j. n. Y H:i",strtotime($clanek->datum)); ?></span>
+                <span class="autor"><i class="fa fa-user"></i><?php echo "$clanek->jmeno $clanek->prijmeni"; ?></span>
+            </div>
+            <?php echo substr($clanek->obsah,0,400)."..."; ?>
+            <div class="cist-dale"><a href="detail-clanku.php?id=<?php echo $clanek->id_clanek; ?>" class="btn">Číst dále</a></div>
+        </div>
     </div>
+    <?php 
+$poc++;
+} 
+?>
 </main>
 <?php 
 //VLOZENI PATICKY
