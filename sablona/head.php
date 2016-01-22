df431bd941e3b09a2c12282b18db5f9fcfae46e1
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-22 10:04:32 +0100

    Favicony

--- sablona/head.php
+++ sablona/head.php
@@ -26,6 +26,30 @@ $domena = ( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  '
 		<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
+		
+		
+		<link rel="apple-touch-icon" sizes="57x57" href="/ikony/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/ikony/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/ikony/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/ikony/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/ikony/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/ikony/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/ikony/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/ikony/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/ikony/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="/ikony/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/ikony/android-chrome-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="/ikony/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="/ikony/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/ikony/manifest.json">
+        <link rel="mask-icon" href="/ikony/safari-pinned-tab.svg" color="#856559">
+        <link rel="shortcut icon" href="/ikony/favicon.ico">
+        <meta name="apple-mobile-web-app-title" content="FotoEx">
+        <meta name="application-name" content="FotoEx">
+        <meta name="msapplication-TileColor" content="#856559">
+        <meta name="msapplication-TileImage" content="/ikony/mstile-144x144.png">
+        <meta name="msapplication-config" content="/ikony/browserconfig.xml">
+        <meta name="theme-color" content="#856559">
 
 	</head>
     <body>
