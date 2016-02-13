7970f0709dd568cf062c95b7e4ca0c144d061eaf
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-02-13 18:50:05 +0100

    Vkládání objednávky do db

--- odeslat.php
+++ odeslat.php
@@ -17,83 +17,43 @@ $id_obj = (((float)$usec + (float)$sec)*10000);
 
 if(isset($_POST["odeslat"])){
     
-    
-    if($_POST["dor_jmeno"])
-        $data_obj["dor_jmeno"] = $_POST["dor_jmeno"];
-    else
-        $data_obj["dor_jmeno"] = $_POST["fak_jmeno"];
-    
-    if($_POST["dor_prijmeni"])
-        $data_obj["dor_prijmeni"] = $_POST["dor_prijmeni"];
-    else
-        $data_obj["dor_prijmeni"] = $_POST["fak_prijmeni"];
-    if($_POST["dor_ulice"])
-        $data_obj["dor_ulice"] = $_POST["dor_ulice"];
-    else
-        $data_obj["dor_ulice"] = $_POST["fak_ulice"];
-    if($_POST["dor_mesto"])
-        $data_obj["dor_mesto"] = $_POST["dor_mesto"];
-    else
-        $data_obj["dor_mesto"] = $_POST["fak_mesto"];
-    if($_POST["dor_psc"])
-        $data_obj["dor_psc"] = $_POST["dor_psc"];
-    else
-        $data_obj["dor_psc"] = $_POST["fak_psc"];
-    if($_POST["dor_zeme"])
-        $data_obj["dor_zeme"] = $_POST["dor_zeme"];
-    else
-        $data_obj["dor_zeme"] = $_POST["fak_zeme"];
-        
-    $data_obj["fak_jmeno"] = $_POST["fak_jmeno"];
-    $data_obj["fak_prijmeni"] = $_POST["fak_prijmeni"];
-    $data_obj["fak_ulice"] = $_POST["fak_ulice"];
-    $data_obj["fak_mesto"] = $_POST["fak_mesto"];
-    $data_obj["fak_psc"] = $_POST["fak_psc"];
-    $data_obj["fak_zeme"] = $_POST["fak_zeme"];
-    
-    $data_obj["uz_email"] = $_POST["uz_email"];
-    $data_obj["uz_telefon"] = $_POST["uz_telefon"];
-    
-    $data_obj["platba"] = $_POST["platba"];
-    $data_obj["doruceni"] = $_POST["doruceni"];
-    
-    if($_POST["doruceni"] == "Kurýr")
-        $data_obj["doruceni_cena"] = 70;
-    if($_POST["doruceni"] == "Česká pošta")
-        $data_obj["doruceni_cena"] = 30;
+    $Kosik = $_SESSION["kosik"];
+    $Kosik->vlozitUdaje();
     
     if(isset($_SESSION["id_uzivatel"])){
         if($data_obj["platba"] == "Převodem na účet")
-            $Objednavky->vlozeni($id_obj,"Čeká se na platbu",$_SESSION["id_uzivatel"],$data_obj);
+            $Objednavky->vlozeni($id_obj,"Čeká se na platbu",$_SESSION["id_uzivatel"],$Kosik->udaje,$Kosik->cena_celkem,$Kosik->platba,$Kosik->doprava);
         else
-            $Objednavky->vlozeni($id_obj,"Zpracovávání",$_SESSION["id_uzivatel"],$data_obj);
+            $Objednavky->vlozeni($id_obj,"Zpracovávání",$_SESSION["id_uzivatel"],$Kosik->udaje,$Kosik->cena_celkem,$Kosik->platba,$Kosik->doprava);
     }
     else{
         if($data_obj["platba"] == "Převodem na účet")
-            $Objednavky->vlozeni($id_obj,"Čeká se na platbu",-1,$data_obj);
+            $Objednavky->vlozeni($id_obj,"Čeká se na platbu",-1,$Kosik->udaje,$Kosik->cena_celkem,$Kosik->platba,$Kosik->doprava);
         else
-            $Objednavky->vlozeni($id_obj,"Zpracovávání",-1,$data_obj);
+            $Objednavky->vlozeni($id_obj,"Zpracovávání",-1,$Kosik->udaje,$Kosik->cena_celkem,$Kosik->platba,$Kosik->doprava);
     }
     
-    
-    foreach($_SESSION["kosik"] as $i => $jedna_fotka){
+    foreach($Kosik->fotky as $i => $jedna_fotka){
         
         //KOPÍROVÁNÍ DO SLOŽKY FOTKY
+        
         if (!file_exists("objednavky"))
             mkdir("objednavky", 0777);
         
         if (!file_exists("objednavky/".$id_obj))
             mkdir("objednavky/".$id_obj, 0777);
         
-        $co = "php/nahrani/tmp-nahrane/".$jedna_fotka["nazev_s"].".".$jedna_fotka["typ_s"];
-        $kam = "objednavky/".$id_obj."/".$jedna_fotka["nazev_s"].".".$jedna_fotka["typ_s"];
+        
+        $co = "php/nahrani/tmp-nahrane/".$jedna_fotka["informace"]["nazev"].".".$jedna_fotka["informace"]["typ_s"];
+        $kam = "objednavky/".$id_obj."/".$jedna_fotka["informace"]["nazev"].".".$jedna_fotka["informace"]["typ_s"];
             
         copy($co,$kam);
         
-        $bez_diakritky = $Fotky->vymazatDiakritiku($jedna_fotka["nazev_s"]);
-        rename("objednavky/".$id_obj."/".$jedna_fotka["nazev_s"].".".$jedna_fotka["typ_s"],"objednavky/".$id_obj."/".$bez_diakritky.".".$jedna_fotka["typ_s"]);
+        $bez_diakritky = $Fotky->vymazatDiakritiku($jedna_fotka["informace"]["nazev"]);
+        rename("objednavky/".$id_obj."/".$jedna_fotka["informace"]["nazev"].".".$jedna_fotka["informace"]["typ_s"],
+               "objednavky/".$id_obj."/".$bez_diakritky.".".$jedna_fotka["informace"]["typ_s"]);
         
-        $data_fot["url"] = $domena."/objednavky/".$id_obj."/".$bez_diakritky.".".$jedna_fotka["typ_s"];
+        $data_fot["url"] = $domena."/objednavky/".$id_obj."/".$bez_diakritky.".".$jedna_fotka["informace"]["typ_s"];
         $url[$i] = $data_fot["url"];
         $data_fot["typ_souboru"] = $jedna_fotka["typ_s"];
         $data_fot["format"] = $jedna_fotka["format"];
@@ -106,10 +66,9 @@ if(isset($_POST["odeslat"])){
         
         $celkem_cena = $celkem_cena + $jedna_fotka["cena_fotka"];
         
-        $Fotky->vlozeni($id_obj,$data_fot);
+       // $Fotky->vlozeni($id_obj,$data_fot);
     }
     
-    $celkem_cena = $celkem_cena + $data_obj["doruceni_cena"];
     
     $zazipovani = new PharData(dirname(__FILE__)."/objednavky/$id_obj/fotografie_$id_obj.zip");
     $zazipovani->buildFromDirectory(dirname(__FILE__)."/objednavky/$id_obj");
@@ -134,33 +93,33 @@ if(isset($_POST["odeslat"])){
             <div class="fakturacni col-md-6">
                 <h2>Fakturační údaje</h2>
                 <ul>
-                    <li><?php echo $data_obj["fak_jmeno"]." ".$data_obj["fak_prijmeni"];?></li>
-                    <li><?php echo $data_obj["fak_ulice"].", ".$data_obj["fak_mesto"];?></li>
-                    <li><?php echo $data_obj["fak_psc"]; ?></li>
-                    <li><?php echo $data_obj["fak_zeme"]; ?></li>
+                    <li><?php echo $Kosik->udaje["fakturacni"]["jmeno"]." ".$Kosik->udaje["fakturacni"]["prijmeni"];?></li>
+                    <li><?php echo $Kosik->udaje["fakturacni"]["ulice"].", ".$Kosik->udaje["fakturacni"]["mesto"];?></li>
+                    <li><?php echo $Kosik->udaje["fakturacni"]["psc"]; ?></li>
+                    <li><?php echo $Kosik->udaje["fakturacni"]["zeme"]; ?></li>
                 </ul>
                 <hr>
                 <h2>Údaje o zákazníkovi</h2>
                 <ul>
-                    <li><a href="mailto:<?php echo $data_obj["uz_email"]; ?>"><?php echo $data_obj["uz_email"]; ?></a></li>
-                    <li><?php echo $data_obj["uz_telefon"]; ?></li>
+                    <li><a href="mailto:<?php echo $Kosik->udaje["uzivatelske"]["email"]; ?>"><?php echo $Kosik->udaje["uzivatelske"]["email"]; ?></a></li>
+                    <li><?php echo $Kosik->udaje["uzivatelske"]["telefon"]; ?></li>
                 </ul>
             </div>
             <div class="dorucovaci col-md-6">
                 <h2>Doručovací údaje</h2>
                 <ul>
-                    <li><?php echo $data_obj["dor_jmeno"]." ".$data_obj["dor_prijmeni"];?></li>
-                    <li><?php echo $data_obj["dor_ulice"].", ".$data_obj["dor_mesto"];?></li>
-                    <li><?php echo $data_obj["dor_psc"]; ?></li>
-                    <li><?php echo $data_obj["dor_zeme"]; ?></li>
+                    <li><?php echo $Kosik->udaje["dorucovaci"]["jmeno"]." ".$Kosik->udaje["dorucovaci"]["prijmeni"];?></li>
+                    <li><?php echo $Kosik->udaje["dorucovaci"]["ulice"].", ".$Kosik->udaje["dorucovaci"]["mesto"];?></li>
+                    <li><?php echo $Kosik->udaje["dorucovaci"]["psc"]; ?></li>
+                    <li><?php echo $Kosik->udaje["dorucovaci"]["zeme"]; ?></li>
                 </ul>
                 <hr>
                 <h2>Platba a doprava</h2>
                 <ul>
-                    <li><strong>Platba:</strong> <?php echo $data_obj["platba"]; ?>
-                    <?php if($data_obj["platba"] == "Převodem na účet") echo "- peníze pošlete na účet: xxxxxxxx/xxxx"; ?>
+                    <li><strong>Platba:</strong> <?php echo $Kosik->platba; ?>
+                    <?php if($Kosik->platba == "Převodem na účet") echo "- peníze pošlete na účet: xxxxxxxx/xxxx"; ?>
                     </li>
-                    <li><strong>Doprava:</strong> <?php echo $data_obj["doruceni"]." (".$data_obj["doruceni_cena"]." Kč)"; ?></li>
+                    <li><strong>Doprava:</strong> <?php echo $Kosik->doprava["typ"]." (".$Kosik->doprava["cena"]." Kč)"; ?></li>
                 </ul>
             </div>
         </div>
@@ -180,7 +139,7 @@ if(isset($_POST["odeslat"])){
             </tr>
         </thead>
         <tbody>
-            <?php foreach($_SESSION["kosik"] as $i => $fotka){ ?>
+            <?php foreach($Kosik->fotky as $i => $fotka){ ?>
                 <tr class="fotka fotka-<?php echo $fotka["id"]; ?>">
                     <td><img src="<?php echo $url[$i]; ?>" height="80"></td>
                     <td><?php echo $fotka["format_nazev"];?></td>
@@ -189,17 +148,17 @@ if(isset($_POST["odeslat"])){
                     <td><?php echo $fotka["deska_nazev"];?></td>
                     <td><?php echo $fotka["typ_nazev"];?></td>
                     <td><?php echo $fotka["pocet"];?>×</td>
-                    <td class="cena"><strong><span><?php echo $fotka["cena_fotka"];?></span> Kč</strong></td>
+                    <td class="cena"><strong><span><?php echo number_format($fotka["cena"], 2, ',', '');?></span> Kč</strong></td>
                 </tr>
             <?php }?>
         </tbody>
         <tfoot>
         <tfoot>
             <tr>
-                <td style="text-align: right;" colspan="8" class="doprava">Cena za dopravu: <?php echo number_format((float)$data_obj["doruceni_cena"], 2, '.', '');?> Kč</td>
+                <td style="text-align: right;" colspan="8" class="doprava">Cena za dopravu: <?php echo number_format((float)$Kosik->doprava["cena"], 2, ',', '');?> Kč</td>
             </tr>
             <tr>
-                <td style="text-align: right;" colspan="8" class="celkem">Celková cena: <span></span> Kč</td>
+                <td style="text-align: right;" colspan="8" class="celkem">Celková cena: <span><?php echo number_format($Kosik->cena_celkem,2); ?></span> Kč</td>
             </tr>
         </tfoot>
         </tfoot>
@@ -207,20 +166,16 @@ if(isset($_POST["odeslat"])){
     </form>
     <img src="img/fotka-udaje.jpg" alt="upload" class="img-responsive" style="margin-top:100px">
 </main>    
+<pre><?php print_r($Kosik); ?></pre>
+
 <pre><?php print_r($_POST); ?></pre>
 <pre><?php print_r($_SESSION); ?></pre>
-<script>
-$(document).ready(function(){
-    var celkem = 0.00;
-    <?php foreach($_SESSION["kosik"] as $fotka){ ?>
-    celkem = celkem + parseFloat($("tbody .fotka-<?php echo $fotka["id"]; ?> td.cena span").text());
-    <?php }?>
-    celkem = celkem + <?php echo (float)$data_obj["doruceni_cena"];?>;
-    $("tfoot td.celkem span").html(celkem.toFixed(2));
-});
-</script>
+
 <?php 
 //VLOZENI PATICKY
+
+
+/*
 
 foreach($_SESSION["kosik"] as $jedna_fotka){
     unlink("php/nahrani/tmp-nahrane/".$jedna_fotka["nazev_s"].".".$jedna_fotka["typ_s"]);
@@ -228,6 +183,8 @@ foreach($_SESSION["kosik"] as $jedna_fotka){
 }
 unset($_SESSION["kosik"]);
 unset($_SESSION["fotky"]);
+*/
+
 require_once("sablona/paticka.php");
 ob_end_flush();
 ?>
