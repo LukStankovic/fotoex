e36e74693aa740e931f752a1c4f1cfed85b539ce
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-02-21 23:30:10 +0100

    Patička včetně copyright

--- sablona/paticka.php
+++ sablona/paticka.php
@@ -10,6 +10,11 @@
                             </ul>
                         </div>
                     </div>
+                    <div class="col-md-9 copyright">
+                        <p>Vytvořil <a href="htttp://www.stankoviclukas.cz" target="_blank">Lukáš Stankovič</a> jako dlouhodobou maturitní práci v školním roce 2015/2016.</p>
+                        <p><a href="/dokumentace/Zadani_I4C_Stankovic_Lukas_115.pdf" target="_blank">Zadání maturitní práce</a> | <a href="#" target="_blank">Dokumentace</a> | <a href="https://github.com/LukStankovic/fotoex" target="_blank">Github</a></p>
+                        <p>Webová stránka neslouží ke komerčním účelům!</p>
+                    </div>
                 </div>
             </div>
         </footer>
