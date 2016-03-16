195e92f15c5885ba2d1c27c85f15cfb9c31f12e4
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-03-16 16:15:29 +0100

    Úprava chyby s odkazem

--- sablona/paticka.php
+++ sablona/paticka.php
@@ -11,7 +11,7 @@
                         </div>
                     </div>
                     <div class="col-md-9 copyright">
-                        <p>Vytvořil <a href="htttp://www.stankoviclukas.cz" target="_blank">Lukáš Stankovič</a> jako dlouhodobou maturitní práci v školním roce 2015/2016.</p>
+                        <p>Vytvořil <a href="http://www.stankoviclukas.cz" target="_blank">Lukáš Stankovič</a> jako dlouhodobou maturitní práci v školním roce 2015/2016.</p>
                         <p><a href="/dokumentace/Zadani_I4C_Stankovic_Lukas_115.pdf" target="_blank">Zadání maturitní práce</a> | <a href="#" target="_blank">Dokumentace</a> | <a href="https://github.com/LukStankovic/fotoex" target="_blank">Github</a></p>
                         <p class="ucely">Webová stránka neslouží ke komerčním účelům!</p>
                     </div>
