bf4346bf74c373a9b2ffa793a6e94e2b6b77f13e
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-03-16 11:51:14 +0100

    Podpora pro IE8

--- sablona/head.php
+++ sablona/head.php
@@ -9,7 +9,7 @@ $domena = ( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  '
 	<head>
 		<meta charset="utf-8">
 		<title>FotoEx - Online fotolab pro Vaše fotografie</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=9" />
+        
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="FotoEx - online fotolab slouží k tisku Vámi nahraných fotografií k tisku. Jedná se pouze o dlouhodobou maturitní práci.">
         <meta name="keywords" content="Fotoex, fotografie, tisk, fotolab">
@@ -30,9 +30,10 @@ $domena = ( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  '
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/stacktable.js"></script>
-		<!--[if IE]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+		<!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 		
 		
 		<link rel="apple-touch-icon" sizes="57x57" href="/ikony/apple-touch-icon-57x57.png">
